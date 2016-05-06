bfda9f779a40b8a2f08371fd899f543f35c3a7cb
Natu Myers <me@natumyers.com>
2016-05-05 23:09:13 -0400

    all set up except db

--- server/app.js
+++ server/app.js
@@ -1,1 +1,14 @@
-import path from 'path'
+// import path from 'path' // use this to test if babel and es feats are working
+
+import express from 'express'
+
+const app = express()
+
+app.get('*', function (req, res) {
+
+    res.send('hello')
+
+})
+
+const port = 3000
+app.listen(port, () => console.log(`Running on ${port}`) )
